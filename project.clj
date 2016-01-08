ae736620210944b2a5333d3e50826e037d06f0be
Vlad Rafeev <vlad19@gmail.com>
2016-01-08 19:22:35 +0200

    new version of Clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [afterglow "0.1.6-SNAPSHOT"]
                  [com.evocomputing/colors "1.0.3"]]
   :main ^:skip-aot lights.core
